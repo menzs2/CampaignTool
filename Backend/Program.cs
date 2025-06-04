c498909d223d5413075f05fae7b720871a87ebaf
77menzs <77menzs@gmail.com>
2025-06-04 16:41:35 +0200

    Add DBcontext

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,9 +1,14 @@
+using Backend;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CampaingToolContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
