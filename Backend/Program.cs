48343ec86e7d6705180c6686a5a5e383ceefbaee
77menzs <77menzs@gmail.com>
2025-06-11 16:01:12 +0200

    Display Name in Swagger UI

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,9 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Campaign Tool API", // <-- Your custom project name
+        Version = "v1",
+        Description = "API documentation for the Campaign Tool project"
+    });
+});
+
 builder.Services.AddDbContext<CampaignToolContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 var app = builder.Build();
 
