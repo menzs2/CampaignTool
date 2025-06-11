46019ffa87d5b542014390ada4fe0f6e2064b9cf
77menzs <77menzs@gmail.com>
2025-06-11 14:42:10 +0200

    Add CharacterDto

---
+++ Shared/DTO/CharacterDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared;
+
+public class CharacterDto
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+    public string? ImageUrl { get; set; }
+    public long CampaignId { get; set; }
+    public long PlayerId { get; set; }
+    public string? GmOnlyDescription { get; set; }
+}
