303af9f499347d91faae96dd45b9125060a7de58
77menzs <77menzs@gmail.com>
2025-06-11 14:15:07 +0200

    Extension class

---
+++ Backend/Data/Extensions.cs
@@ -0,0 +1,46 @@
+﻿using Backend.Models;
+
+namespace Shared;
+
+public static class Extensions
+{
+    public static CampaignDto ToDto(this Campaign campaign)
+    {
+        if (campaign == null) return null;
+
+        return new CampaignDto
+        {
+            Id = campaign.Id,
+            CampaignName = campaign.CampaignName,
+            DescriptionShort = campaign.DescriptionShort,
+            Description = campaign.Description,
+            Gm = campaign.Gm,
+            GmOnlyDescription = campaign.GmOnlyDescription
+        };
+    }
+
+    public static IEnumerable<CampaignDto> ToDto(this IEnumerable<Campaign> campaigns)
+    {
+        return campaigns?.Select(c => c.ToDto()) ?? Enumerable.Empty<CampaignDto>();
+    }
+
+    public static Campaign ToModel(this CampaignDto campaignDto)
+    {
+        if (campaignDto == null) return null;
+
+        return new Campaign
+        {
+            Id = campaignDto.Id,
+            CampaignName = campaignDto.CampaignName,
+            DescriptionShort = campaignDto.DescriptionShort,
+            Description = campaignDto.Description,
+            Gm = campaignDto.Gm,
+            GmOnlyDescription = campaignDto.GmOnlyDescription
+        };
+    }
+
+    public static IEnumerable<Campaign> ToModel(this IEnumerable<CampaignDto> campaignDtos)
+    {
+        return campaignDtos?.Select(c => c.ToModel()) ?? Enumerable.Empty<Campaign>();
+    }
+}
