cc6fcf7c4534ceb7cee4eedc14051bf6e8e23245
77menzs <77menzs@gmail.com>
2025-06-11 15:45:45 +0200

    Update OrganisationController

--- Backend/Controllers/OrganisationController.cs
+++ Backend/Controllers/OrganisationController.cs
@@ -1,5 +1,6 @@
 ﻿using Backend.Data;
 using Microsoft.AspNetCore.Mvc;
+using Shared;
 namespace Backend;
 
 [Route("api/[controller]")]
@@ -17,21 +18,21 @@ public class OrganisationController : ControllerBase
     public IActionResult Get()
     {
         return _dbContext.Organisations.Any()
-            ? Ok(_dbContext.Organisations.ToList())
+            ? Ok(_dbContext.Organisations.ToDto())
             : NotFound("No organisations found.");
     }
 
     [HttpGet("{id}")]
     public IActionResult Get(long id)
     {
-        var organisation = _dbContext.Organisations.Find(id);
+        var organisation = _dbContext.Organisations.Where(o=> o.Id == id).FirstOrDefault().ToDto();
         return organisation != null ? Ok(organisation) : NotFound($"Organisation with ID {id} not found.");
     }
 
     [HttpGet("campaign/{campaignId}")]
     public IActionResult GetByCampaign(long campaignId)
     {
-        var organisations = _dbContext.Organisations.Where(o => o.CampaignId == campaignId).ToList();
+        var organisations = _dbContext.Organisations.Where(o => o.CampaignId == campaignId).ToDto();
         return organisations.Any() ? Ok(organisations) : NotFound($"No organisations found for campaign ID {campaignId}.");
     }
 
@@ -40,25 +41,29 @@ public class OrganisationController : ControllerBase
     {
         var organisations = _dbContext.Organisations
             .Where(o => o.Id != id && o.CharOrgConnections.Any(c => c.OrganisationId == id))
-            .ToList();
+            .ToDto();
         return organisations.Any() ? Ok(organisations) : NotFound("No connected organisations found.");
     }
 
     [HttpPost]
-    public IActionResult Post([FromBody] Models.Organisation organisation)
+    public IActionResult Post([FromBody] OrganisationDto? organisation)
     {
         if (organisation == null)
         {
             return BadRequest("Organisation data is null.");
         }
-
-        _dbContext.Organisations.Add(organisation);
+        var newOrganisation = organisation.ToModel();
+        if (newOrganisation == null)
+        {
+            return BadRequest("Invalid organisation data.");
+        }
+        _dbContext.Organisations.Add(newOrganisation);
         _dbContext.SaveChanges();
         return CreatedAtAction(nameof(Get), new { id = organisation.Id }, organisation);
     }
 
     [HttpPut("{id}")]
-    public IActionResult Put(long id, [FromBody] Models.Organisation organisation)
+    public IActionResult Put(long id, [FromBody] OrganisationDto? organisation)
     {
         if (organisation == null || organisation.Id != id)
         {
