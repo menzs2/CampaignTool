3597a4be5a13e61e1d45f6a8bd9d242674079234
77menzs <77menzs@gmail.com>
2025-07-24 16:23:12 +0200

    Implement OrganisationService

--- Backend/Services/OrganisationService.cs
+++ Backend/Services/OrganisationService.cs
@@ -1,5 +1,4 @@
 ﻿using Backend.Data;
-using Backend.Models;
 using Microsoft.EntityFrameworkCore;
 using Shared;
 
@@ -19,7 +18,7 @@ public class OrganisationService
         var organisations = await _context.Organisations.ToListAsync();
         return organisations?.ToDto();
     }
-    
+
     public async Task<IEnumerable<OrganisationDto>?> GetOrganisationDtoAsync(long campaignId)
     {
         var organisations = await _context.Organisations.Where(o => o.CampaignId == campaignId).ToListAsync();
@@ -53,5 +52,52 @@ public class OrganisationService
         return organisations?.ToDto();
     }
 
+    public async Task<OrganisationDto?> CreateOrganisation(OrganisationDto organisationDto)
+    {
+        if (organisationDto == null)
+        {
+            throw new ArgumentNullException(nameof(OrganisationDto), "Connection DTO cannot be null");
+        }
+        var organisation = organisationDto.ToModel();
+        if (organisation == null)
+        {
+            throw new InvalidOperationException("Failed to convert ConnectionDto to Connection model");
+        }
+        _context.Organisations.Add(organisation);
+        await _context.SaveChangesAsync();
+        return organisation?.ToDto();
+    }
+
+    public async Task<OrganisationDto?> UpdateOrganisation(long id, OrganisationDto organisationDto)
+    {
+        if (organisationDto == null)
+        {
+            throw new ArgumentNullException(nameof(organisationDto), "Organisation DTO cannot be null");
+        }
+        var existingOrganisation = await _context.Organisations.FindAsync(id);
+        if (existingOrganisation == null)
+        {
+            throw new KeyNotFoundException($"Organisation with ID '{id}' not found.");
+        }
+        existingOrganisation.Description = organisationDto.Description;
+        existingOrganisation.DescriptionShort = organisationDto.DescriptionShort;
+        existingOrganisation.GmOnly = organisationDto.GmOnly;
+        existingOrganisation.GmOnlyDescription = existingOrganisation.GmOnlyDescription;
+
+        _context.Organisations.Update(existingOrganisation);
+        await _context.SaveChangesAsync();
+        return existingOrganisation?.ToDto();
+    }
+
+    public async Task DeleteOrganisation(long id)
+    {
+        var existingOrganisation = await _context.Organisations.FindAsync(id);
+        if (existingOrganisation == null)
+        {
+            throw new KeyNotFoundException($"Organisation with id '{id}' not found.");
+        }
+        _context.Remove(existingOrganisation);
+        await _context.SaveChangesAsync();
+    }
 }
 
