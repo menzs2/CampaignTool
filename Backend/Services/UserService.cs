157c62184ee7828a787c72c510e5dc28ebf5456a
77menzs <77menzs@gmail.com>
2025-07-25 15:55:20 +0200

    Implement User methods

--- Backend/Services/UserService.cs
+++ Backend/Services/UserService.cs
@@ -1,4 +1,5 @@
-﻿using Backend.Data;
+﻿using Backend.Models;
+using Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Shared;
 
@@ -11,6 +12,69 @@ namespace Backend
         public UserService(CampaignToolContext context)
         {
             _context = context;
+        }
+
+        public async Task<IEnumerable<UserDto>> GetAllUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users.ToDto();
+        }
+
+        public async Task<UserDto?> GetUserByID(long id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            return user?.ToDto();
+        }
+
+        public async Task<UserDto?> CreateUser(UserDto userDto)
+        {
+            if (userDto == null)
+            {
+                throw new ArgumentNullException(nameof(userDto), "Character data is null.");
+            }
+            var newUser = userDto.ToModel();
+            if (newUser == null)
+            {
+                throw new InvalidOperationException("Failed to convert UserDTo to User model.");
+            }
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+            // Automatically create a UserSetting for the new user
+            var usersetting = new UserSetting
+            {
+                UserId = newUser.Id,
+                DefaultCampaignId = null,
+                SelectLastCampaign = true,
+                SameNameWarning = true
+            };
+            _context.UserSettings.Add(usersetting);
+            await _context.SaveChangesAsync();
+            return newUser?.ToDto();
+        }
+
+        public async Task UpdateUser(long id, UserDto userDto)
+        {
+            var exisitingUser = await _context.Users.FindAsync(id);
+            if (exisitingUser == null)
+            {
+                throw new KeyNotFoundException($"User with id '{id}' not found.");
+            }
+            exisitingUser.FirstName = userDto.FirstName;
+            exisitingUser.LastName = userDto.LastName;
+            exisitingUser.HasLogin = userDto.HasLogin;
+            _context.Update(exisitingUser);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteUser(long id)
+        {
+            var exisitingUser = await _context.Users.FindAsync(id);
+            if (exisitingUser == null)
+            {
+                throw new KeyNotFoundException($"User with id '{id}' not found.");
+            }
+            _context.Remove(exisitingUser);
+            await _context.SaveChangesAsync();
         }
     }
 }
