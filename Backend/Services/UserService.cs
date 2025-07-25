7958f002c8df12a7060e8da8c8fc49029105e152
77menzs <77menzs@gmail.com>
2025-07-25 15:33:11 +0200

    Add UserService

---
+++ Backend/Services/UserService.cs
@@ -0,0 +1,16 @@
+﻿using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+using Shared;
+
+namespace Backend
+{
+    public class UserService
+    {
+        private readonly CampaignToolContext _context;
+
+        public UserService(CampaignToolContext context)
+        {
+            _context = context;
+        }
+    }
+}
