a6f920be80242aae56d3f80b227afc9eabddadeb
77menzs <77menzs@gmail.com>
2025-07-28 13:41:03 +0200

    Add UserServiceTest methods and correct found errors

--- Backend.Tests/UserServiceTest.cs
+++ Backend.Tests/UserServiceTest.cs
@@ -2,23 +2,137 @@ using Backend.Data;
 using Backend.Models;
 using Shared;
 using Microsoft.EntityFrameworkCore;
-using Backend.Services;
 
 namespace Backend.Tests
 {
-    public class UserTest
+    public class UserServiceTest
     {
+        private List<User> Users => new List<User>
+        {
+            new User { Id = 1L,
+                FirstName = "Jean-Helmut",
+                LastName = "Muster",
+                Email = "j.m@example.com",
+                UserName = "jhm",
+                HasLogin = true, Password = "Password123",
+                UserSetting =  new UserSetting { Id = 1L, UserId = 1, DefaultCampaignId = null, SameNameWarning = true, SelectLastCampaign = true }},
+            new User { Id = 2L,
+                FirstName = "Phillippa",
+                LastName = "Marker",
+                UserName = "pm",
+                Email = "P.m@example.com",
+                HasLogin = true,
+                Password = "Password456",
+                UserSetting = new UserSetting { Id = 2L, UserId = 2, DefaultCampaignId = null, SameNameWarning = false, SelectLastCampaign = true } }
+        };
+
+
         CampaignToolContext GetDbContextWithData(List<User> users)
         {
             var options = new DbContextOptionsBuilder<CampaignToolContext>()
                 .UseInMemoryDatabase(databaseName: "TestDb" + Guid.NewGuid())
                 .Options;
             var context = new CampaignToolContext(options);
-            context.Users.AddRange(users);
+            foreach (var user in users)
+            {
+                context.Users.Add(user);
+            }
             context.SaveChanges();
             return context;
         }
 
+        [Fact]
+        public async Task Get_ReturnsUser_WhenExist()
+        {
+            var context = GetDbContextWithData(Users);
+            var service = new UserService(context);
 
+            var result = await service.GetAllUsers();
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.IsType<List<UserDto>>(result);
+            Assert.Equal(2, result.Count());
+        }
+
+        [Fact]
+        public async Task Get_ReturnEmpty_WhenNotExist()
+        {
+            var context = GetDbContextWithData(new List<User> { });
+            var service = new UserService(context);
+
+            var result = await service.GetAllUsers();
+
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public async Task GetById_ReturnUser_WhenExist()
+        {
+            var context = GetDbContextWithData(Users);
+            var service = new UserService(context);
+
+            var result = await service.GetUserByID(1);
+
+            Assert.NotNull(result);
+            Assert.IsType<UserDto>(result);
+            Assert.Equal(Users[0].LastName, result.LastName);
+        }
+
+        [Fact]
+        public async Task GetById_ReturnNull_WhenNotExtist()
+        {
+            var context = GetDbContextWithData(new List<User> { });
+            var service = new UserService(context);
+
+            var result = await service.GetUserByID(999);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task CreateUser_ReturnObject_WhenValid()
+        {
+            var context = GetDbContextWithData(Users);
+            var service = new UserService(context);
+            var newUser = new User
+            {
+                FirstName = "Alan",
+                LastName = "marker",
+                Email = "a.m@example.com",
+                UserName = "am",
+                HasLogin = true,
+                Password = "Password789",
+                UserSetting = new UserSetting { Id = 3L, UserId = 3, DefaultCampaignId = null, SameNameWarning = true, SelectLastCampaign = true }
+            };
+            var result = await service.CreateUser(newUser.ToDto());
+
+            Assert.NotNull(result);
+            Assert.Equal(result.LastName, newUser.LastName);
+        }
+
+        [Fact]
+        public async Task CreateUser_ReturnUserSetting_WhenCreated()
+        {
+            var context = GetDbContextWithData(Users);
+            var service = new UserService(context);
+            var newUser = new User
+            {
+                FirstName = "Alan",
+                LastName = "marker",
+                Email = "a.m@example.com",
+                UserName = "am",
+                HasLogin = true,
+                Password = "Password789",
+                UserSetting = new UserSetting { Id = 3L, UserId = 3, DefaultCampaignId = null, SameNameWarning = true, SelectLastCampaign = true }
+            };
+            await service.CreateUser(newUser.ToDto());
+            var result = await service.GetUserSettingById(3);
+
+            Assert.NotNull(result);
+            Assert.IsType<UserSettingDto>(result);
+            Assert.Equal(result.SameNameWarning, newUser.UserSetting.SameNameWarning);
+        }
     }
 }
