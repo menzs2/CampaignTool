0206109ab462567c2f995db339a94ad831796d9c
77menzs <77menzs@gmail.com>
2025-07-31 11:54:37 +0200

    Add data service class

---
+++ Frontend/Services/BaseDataService.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using Shared;
+
+namespace Frontend;
+
+public abstract class BaseDataService
+{
+    protected HttpClient HttpClient { get; set; }
+
+    protected JsonSerializerOptions SerializerOptions {get; set;}
+
+    public BaseDataService(HttpClient httpClient)
+    {
+        HttpClient = httpClient;
+        SerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+    }
+}
---
+++ Frontend/Services/CampaignDataService.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using Shared;
+
+namespace Frontend;
+
+public class CampaignDataService : BaseDataService
+{
+    public CampaignDataService(HttpClient httpClient) : base(httpClient){}
+
+    public async Task<List<CampaignDto>?> GetCampaigns()
+    {
+        try
+        {
+            var response = await HttpClient.GetAsync("http://localhost:5043/api/campaign");
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync();
+            var campaigns = JsonSerializer.Deserialize<List<CampaignDto>>(json, SerializerOptions);
+            return campaigns;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        return null;
+    }
+}
