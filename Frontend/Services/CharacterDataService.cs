3997e07751c3a7b7a1c974167be35a41ef3836ea
77menzs <77menzs@gmail.com>
2025-07-31 12:52:08 +0200

    Add CharacterDataService

---
+++ Frontend/Services/CharacterDataService.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using Shared;
+
+namespace Frontend;
+
+public class CharacterDataService : BaseDataService
+{
+    public CharacterDataService(HttpClient httpClient) : base(httpClient) { }
+
+    public async Task<List<CharacterDto>?> GetCharacterDtosAsync()
+    {
+        try
+        {
+            var response = await HttpClient.GetAsync("http://localhost:5043/api/character");
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync();
+            var characters = JsonSerializer.Deserialize<List<CharacterDto>>(json, SerializerOptions);
+            return characters;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        return null;
+    }
+}
