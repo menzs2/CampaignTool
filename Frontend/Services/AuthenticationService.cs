907a8e6ccbb9737397444648fca703246196109b
77menzs <77menzs@gmail.com>
2025-08-15 14:20:46 +0200

    Add authentication service

---
+++ Frontend/Services/AuthenticationService.cs
@@ -0,0 +1,42 @@
+﻿using Shared.DTO;
+using System.Net.Http.Json;
+
+namespace Frontend
+{
+    public class AuthenticationService : BaseDataService
+    {
+        public AuthenticationService(HttpClient httpClient) : base(httpClient){}
+
+        private readonly string baseRoute = "api/auth";
+
+        public async Task<bool> LoginAsync(string username, string password)
+        {
+            try
+            {
+                var response = await HttpClient.PostAsJsonAsync($"{HttpClient.BaseAddress}{baseRoute}/login", new { username, password });
+                response.EnsureSuccessStatusCode();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Login Error: {ex.Message}");
+                return false;
+            }
+        }
+
+        public async Task<bool> RegisterAsync(RegisterRequestWithRoleDto registerRequest)
+        {
+            try
+            {
+                var response = await HttpClient.PostAsJsonAsync($"{HttpClient.BaseAddress}{baseRoute}/register", registerRequest);
+                response.EnsureSuccessStatusCode();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Registration Error: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
