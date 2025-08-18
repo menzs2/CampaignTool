716c663dfb02c8e8837327c62b170becabdc41e3
77menzs <77menzs@gmail.com>
2025-08-18 09:59:34 +0200

    Corrections and improvements

---
+++ Frontend/Services/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace Frontend.Services
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public CustomAuthenticationStateProvider(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            string? token = await _jsRuntime.InvokeAsync<string>("sessionStorage.getItem", "authToken");
+            var identity = string.IsNullOrWhiteSpace(token) ? new ClaimsIdentity() : new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+            var user = new ClaimsPrincipal(identity);
+            return new AuthenticationState(user);
+        }
+        public bool IsAuthenticated()
+        {
+            return GetAuthenticationStateAsync().Result is AuthenticationState state && state.User.Identity != null && state.User.Identity.IsAuthenticated;
+        }
+
+
+        private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+            return keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
+        }
+
+        private byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+            }
+            return Convert.FromBase64String(base64);
+        }
+    }
+}
